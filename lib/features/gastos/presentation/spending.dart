b356183c77e6324e34572397772ad94d4a1e0b90
Heloisa Ribeiro <hribe.silva@gmail.com>
2026-04-26 12:35:04 -0300

    feat: cria estrutura inicial da tela de gastos

---
+++ lib/features/gastos/presentation/spending.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart' hide SearchBar;
+import 'package:invezzte/core/widgets/NavBar.dart';
+
+class Spending extends StatelessWidget {
+  const Spending({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: const Color(0xFFFAFAFA),
+      body: const SafeArea( //Para não sobrepor a barra do celular que aparece bateria, hora, etc.
+        top: false,
+        child: SingleChildScrollView( //PAra rolar a tela inteira verticalmente
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch, //Para ocupar toda a largura da tela
+            children: [
+              //Aqui irei colocar os Widgets
+
+              //1. Cabeçalho (SpendingHeader)
+              //2. Filtros (SpendingFilterList)
+              //3. Data (SpendingDateHeader)
+              //4. Transações (SpendingTransactionsList)
+
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: NavBar(),
+    );
+  }
+}
